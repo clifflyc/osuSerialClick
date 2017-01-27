42ef09603763a1e517930576256a49763e8a1a6a
Cliff <clifflyc@gmail.com>
2017-01-26 21:39:16 -0500

    cleaned up code in arduino file

--- arduino/osuClick/osuClick.ino
+++ arduino/osuClick/osuClick.ino
@@ -1,31 +1,34 @@
-const int read1=8;
-const int read2=9;
-boolean pressed1=false;
-boolean pressed2=false;
-
+const byte INPUT_A = 8, INPUT_B = 9; //pin numbers for two input buttons
+//const byte LED_R = 13, LED_G = 12, LED_B = 11; //pin numbers for RGB led
+bool pressedA = false, pressedB = false; //is button A/B currently pressed down already?
+bool pressingA, pressingB; //is pressing button A/B currently?
 
 void setup() {
   Serial.begin(9600);
-  //set up both read pins (with pull-up resistors)
-  pinMode(read1,INPUT);
-  pinMode(read2,INPUT);
-  digitalWrite(read1,HIGH);
-  digitalWrite(read2,HIGH);  
+  pinMode(INPUT_A, INPUT_PULLUP);
+  pinMode(INPUT_B, INPUT_PULLUP);
+  //pinMode(LED_R,OUTPUT);
+  //pinMode(LED_G,OUTPUT);
+  //pinMode(LED_B,OUTPUT);
 }
 
 void loop() {
-  if(digitalRead(read1)==LOW && !pressed1){
-    pressed1=true;
+  bool pressingA = !digitalRead(INPUT_A);
+  bool pressingB = !digitalRead(INPUT_B);
+
+  if (pressingA && !pressedA) { //button A key down
+    pressedA = true;
     Serial.write("z");
-  } else if(digitalRead(read1)==HIGH && pressed1){
-    pressed1=false;
+  } else if (!pressingA && pressedA) { //button A key up
+    pressedA = false;
     Serial.write("a");
   }
-  if(digitalRead(read2)==LOW && !pressed2){
-    pressed2=true;
+  if (pressingB && !pressedB) { //button B key down
+    pressedB = true;
     Serial.write("x");
-  } else if(digitalRead(read2)==HIGH && pressed2){
-    pressed2=false;
+  } else if (!pressingB && pressedB) { //button B key up
+    pressedB = false;
     Serial.write("s");
   }
+  delay(1);
 }
