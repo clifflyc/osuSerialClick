535d28e986303ead6337104dd379ddee88a7cf13
Cliff <clifflyc@gmail.com>
2017-02-02 22:21:25 -0500

    added LED feature to arduino code
    for fun

--- arduino/osuClick/osuClick.ino
+++ arduino/osuClick/osuClick.ino
@@ -1,15 +1,17 @@
 const byte INPUT_A = 8, INPUT_B = 9; //pin numbers for two input buttons
-//const byte LED_R = 13, LED_G = 12, LED_B = 11; //pin numbers for RGB led
+const byte LED_PINS [3] = {12, 11, 10}; //pin numbers for RGB led = {R,G,B}
+const float LED_ALPHA = 20, LED_FADE_SPEED = 0.2; //led strength and fade out speed
 bool pressedA = false, pressedB = false; //is button A/B currently pressed down already?
-bool pressingA, pressingB; //is pressing button A/B currently?
+float currentAlpha [3] = {0, 0, 0}; //used to store current alpha amount for led rgb pins //{R,G,B}
+
 
 void setup() {
   Serial.begin(9600);
   pinMode(INPUT_A, INPUT_PULLUP);
   pinMode(INPUT_B, INPUT_PULLUP);
-  //pinMode(LED_R,OUTPUT);
-  //pinMode(LED_G,OUTPUT);
-  //pinMode(LED_B,OUTPUT);
+  for (int i = 0; i < 3; i++) {
+    pinMode(LED_PINS[i], HIGH);
+  }
 }
 
 void loop() {
@@ -18,17 +20,49 @@ void loop() {
 
   if (pressingA && !pressedA) { //button A key down
     pressedA = true;
-    Serial.write("z");
+    lightLED(1);
+    Serial.write('z');
   } else if (!pressingA && pressedA) { //button A key up
     pressedA = false;
-    Serial.write("a");
+    Serial.write('a');
   }
   if (pressingB && !pressedB) { //button B key down
     pressedB = true;
-    Serial.write("x");
+    lightLED(2);
+    Serial.write('x');
   } else if (!pressingB && pressedB) { //button B key up
     pressedB = false;
-    Serial.write("s");
+    Serial.write('s');
   }
+  updateAlpha(pressingA, pressingB);
   delay(1);
 }
+
+void lightLED(int index) {
+  currentAlpha[index] += LED_ALPHA;
+  if (currentAlpha[index] > 255) {
+    currentAlpha[index] = 255;
+  }
+  setLED(index);
+}
+void updateAlpha(bool pressingA, bool pressingB) {
+  if (!pressingA) {
+    if (currentAlpha[1] > 0) {
+      currentAlpha[1] -= LED_FADE_SPEED;
+    }
+    setLED(1);
+  }
+  if (!pressingB) {
+    if (currentAlpha[2] > 0) {
+      currentAlpha[2] -= LED_FADE_SPEED;
+    }
+    setLED(2);
+  }
+
+}
+
+void setLED(int index) {
+  float alpha = (int) currentAlpha[index];
+  analogWrite(LED_PINS[index], alpha);
+}
+
